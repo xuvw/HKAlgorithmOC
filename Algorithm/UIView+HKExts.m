3c9cafb095910ca67538b1a484339fe116827ffa
heke <heke@soyoung.com>
2016-03-29 10:34:58 +0800

    add DFS search

--- Algorithm/UIView+HKExts.m
+++ Algorithm/UIView+HKExts.m
@@ -52,11 +52,43 @@
 
 @interface NSMutableArray (HKExtsStack)
 
+- (void)clearStack;
+- (BOOL)isStackEmpty;
+- (NSInteger)stackLength;
+
+- (void)push:(id)item;
+- (id)popItem;
+- (id)topItem;
+- (id)bottomItem;
+
 @end
 
 @implementation NSMutableArray (HKExtsStack)
 
+- (void)clearStack {
+    [self removeAllObjects];
+}
+- (BOOL)isStackEmpty {
+    return [self count]<1;
+}
+- (NSInteger)stackLength {
+    return [self count];
+}
 
+- (void)push:(id)item {
+    [self addObject:item];
+}
+- (id)popItem {
+    id item = [self lastObject];
+    [self removeLastObject];
+    return item;
+}
+- (id)topItem {
+    return [self lastObject];
+}
+- (id)bottomItem {
+    return [self firstObject];
+}
 
 @end
 
@@ -64,6 +96,8 @@
 
 /*
  BFS
+ 2016-03-29 10:32:24.425 HKAlgorithmOC[60735:660819] search time :6
+ 2016-03-29 10:32:24.426 HKAlgorithmOC[60735:660819] search time :7
  */
 - (UIView *)subViewOfClass:(NSString *)aClassName {
     
@@ -80,8 +114,9 @@
     
     __block NSArray *subViews = nil;
     __block UIView  *aView = nil;
-    
+    NSInteger count = 0;
     while (![queue isQueueEmpty]) {
+        count++;
         aView = [queue frontItem];
         subViews = [aView subviews];
         aView = nil;
@@ -116,8 +151,69 @@
     
     [queue clearQueue];
     
+    NSLog(@"search time :%ld",count);
     return targetView;
 }
+
+/*
+ DFS
+ 2016-03-29 10:31:10.960 HKAlgorithmOC[60713:660098] search time :8
+ 2016-03-29 10:31:10.960 HKAlgorithmOC[60713:660098] search time :10
+ */
+//- (UIView *)subViewOfClass:(NSString *)aClassName {
+//    
+//    if (aClassName.length<1) {
+//        return nil;
+//    }
+//    
+//    Class class = NSClassFromString(aClassName);
+//    __block UIView *targetView = nil;
+//    
+//    NSMutableArray *visitedViews = [NSMutableArray array];
+//    NSMutableArray *stack = [NSMutableArray array];
+//    __block NSArray *subViews = nil;
+//    __block UIView  *aView    = nil;
+//    
+//    self.visited = YES;
+//    [stack push:self];
+//    NSInteger count = 0;
+//    while (![stack isStackEmpty]) {
+//        count ++;
+//        aView = [stack topItem];
+//        subViews = [aView subviews];
+//        aView = nil;
+//        [subViews enumerateObjectsUsingBlock:^(UIView *  _Nonnull aSubView, NSUInteger idx, BOOL * _Nonnull stop) {
+//            if (!aSubView.visited) {
+//                aView = aSubView; *stop = YES;
+//            }
+//        }];
+//        
+//        if (aView) {
+//            if ([aView isKindOfClass:class]) {
+//                targetView = aView;
+//                break;
+//            }else{
+//                aView.visited = YES;
+//                [stack push:aView];
+//            }
+//        }else{
+//            [visitedViews addObject:[stack popItem]];
+//        }
+//    }
+//    
+//    [visitedViews enumerateObjectsUsingBlock:^(UIView *  _Nonnull aSubView, NSUInteger idx, BOOL * _Nonnull stop) {
+//        aSubView.visited = NO;
+//    }];
+//    
+//    [stack enumerateObjectsUsingBlock:^(UIView *  _Nonnull aSubView, NSUInteger idx, BOOL * _Nonnull stop) {
+//        aSubView.visited = NO;
+//    }];
+//    
+//    [visitedViews removeAllObjects]; [stack removeAllObjects];
+//    
+//    NSLog(@"search time :%ld",count);
+//    return targetView;
+//}
 
 #pragma mark - private
 - (void)setVisited:(BOOL)visited {
